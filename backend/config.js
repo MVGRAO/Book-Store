1856e33f0f1722c1da1bc396d9268bd60f87bebc
MVGRAO <programchewer100@gmail.com>
2025-05-12 16:17:19 +0530

    Initial commit

---
+++ backend/config.js
@@ -0,0 +1,6 @@
+export const PORT =5555;
+
+export const mongoDBURL=
+    'mongodb+srv://n201056:Mvgrao@2005@book-store.wxlmyok.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store'
+
+    
